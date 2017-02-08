ea306b30dc2816b8d8d783ca788d9086b3f4bddd
TheMHMoritz3 <mhmoritz3@gmail.com>
2017-02-08 21:09:22 +0100

    Removed Material Design
    Maybe this fixes Travis

--- src/View/WindowsClient/MainWindow.xaml.cs
+++ src/View/WindowsClient/MainWindow.xaml.cs
@@ -28,8 +28,8 @@ namespace WindowsClient
         {
             InitializeComponent();
             createInstanceManager();
-            Header.Text = Manager.generateMenuManager().WidgetArray[0].Title;
-            Panel.Children.Add(Manager.generateMenuManager().getContentOfCurrentItem());
+            //Header.Text = Manager.generateMenuManager().WidgetArray[0].Title;
+            //Panel.Children.Add(Manager.generateMenuManager().getContentOfCurrentItem());
             IConnection connection = new IConnection();
             //Thread ConnectionThread = new Thread(()=>connection.openConnection("jabber.german-software-engineers.com", 5222, new LoginData("the_mhmoritz3","", "german-software-engineers.com")));
             //ConnectionThread.Start();
@@ -37,25 +37,25 @@ namespace WindowsClient
 
         public void createInstanceManager()
         {
-            Manager = new InstanceManager();
-            ViewSliderItem[] items = Manager.generateMenuManager().WidgetArray;
-            foreach (ViewSliderItem item in items)
-                ListOfContent.Items.Add(item.Title);
+            //Manager = new InstanceManager();
+            //ViewSliderItem[] items = Manager.generateMenuManager().WidgetArray;
+            //foreach (ViewSliderItem item in items)
+                //ListOfContent.Items.Add(item.Title);
         }
 
         private InstanceManager Manager;
 
         private void ListOfContent_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int selectedIndex = ListOfContent.SelectedIndex;
+            //int selectedIndex = ListOfContent.SelectedIndex;
 
-            if (selectedIndex < 0)
-                return;
+            //if (selectedIndex < 0)
+            //    return;
 
-            Panel.Children.Remove(Manager.generateMenuManager().getContentOfCurrentItem());
-            Manager.generateMenuManager().setCurrentItem(selectedIndex);
-            Header.Text = Manager.generateMenuManager().WidgetArray[selectedIndex].Title;
-            Panel.Children.Add(Manager.generateMenuManager().getContentOfCurrentItem());
+            //Panel.Children.Remove(Manager.generateMenuManager().getContentOfCurrentItem());
+            //Manager.generateMenuManager().setCurrentItem(selectedIndex);
+            //Header.Text = Manager.generateMenuManager().WidgetArray[selectedIndex].Title;
+            //Panel.Children.Add(Manager.generateMenuManager().getContentOfCurrentItem());
             //Controls.Add(newPanelButton); Manager.getContentOfCurrentItem();
         }
     }
