e70e4efe7f383179705af4d24077284080a77152
TheMHMoritz3 <mhmoritz3@outlook.com>
2017-02-09 20:22:16 +0100

    Started Work on Linux Window

---
+++ src/View/LinuxClient/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinuxClient
+{
+	public partial class MainWindow : Gtk.Window
+	{
+		public MainWindow () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+			UserView = new Gtk.TreeView ();
+			Gtk.TreeViewColumn user = new Gtk.TreeViewColumn ();
+			user.Title = "TestUser";
+			UserView.AppendColumn (user);
+			frame1.Add (UserView);
+		}
+
+
+		Gtk.TreeView UserView;
+	}
+}
+
