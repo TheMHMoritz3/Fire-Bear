078333b2d1a693c94b5f4fdd86ad0484725d5495
TheMHMoritz3 <mhmoritz3@outlook.com>
2017-02-12 18:43:49 +0100

    Test Linux Tests With Windows

---
+++ src/UnitTests/LinuxTest/Data/ContactTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Model.Data;
+using NUnit.Framework;
+using System.Runtime.InteropServices;
+
+namespace TestModel.Data
+{
+    [TestFixture]
+    class ContactTest
+    {
+        [Test]
+        public void testConstructor()
+        {
+            Contact contact = new Contact("fIREbEAR","german-software-engineers.com");
+            Assert.That(contact.UserName, Is.EqualTo("fIREbEAR"));
+            Assert.That(contact.Server, Is.EqualTo("german-software-engineers.com"));
+        }
+
+        [Test]
+        public void testPropertieUserName()
+        {
+            Contact contact = new Contact();
+            contact.UserName = "fIREbEAR";
+            Assert.That(contact.UserName, Is.EqualTo("fIREbEAR"));
+        }
+
+        [Test]
+        public void testPropertieServer()
+        {
+            Contact contact = new Contact();
+            contact.Server = "german-software-engineers.com";
+            Assert.That(contact.Server, Is.EqualTo("german-software-engineers.com"));
+        }
+    }
+}
---
+++ src/UnitTests/LinuxTest/Data/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace LinuxTest
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
