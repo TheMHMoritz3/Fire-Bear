45fc380b7e04e4c414573fea77c00132bf23f9f6
TheMHMoritz3 <mhmoritz3@gmail.com>
2017-02-12 13:25:25 +0100

    Connected Login Data with Windows

--- src/View/WindowsClient/MainWindow.xaml.cs
+++ src/View/WindowsClient/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Model.Manager.States;
+using Model.Data;
 
 namespace WindowsClient
 {
@@ -19,20 +21,43 @@ namespace WindowsClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Is Triggered if Close Action is Triggered
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuItem_CloseClick(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// On Login Clicked is a LoginData Creted and a new Connection is Created
+        /// TODO: Create new Connection.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuItem_LoginClick(object sender, RoutedEventArgs e)
         {
             LoginDialog.LoginDialog loginDialog = new LoginDialog.LoginDialog();
             loginDialog.ShowDialog();
+            if(loginDialog.getStatusManager().getState()==1)
+            {
+                _CurrentLoginData = loginDialog.getLogin();
+            }
         }
+
+        /// <summary>
+        /// The Current Login Data
+        /// </summary>
+        private LoginData _CurrentLoginData = null;
     }
 }
