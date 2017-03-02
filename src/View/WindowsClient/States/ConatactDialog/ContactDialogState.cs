b847aa659579b580188f39ef4a68696487544999
TheMHMoritz3 <mhmoritz3@outlook.com>
2017-03-02 18:38:01 +0100

    Add User Dialog Added

---
+++ src/View/WindowsClient/States/ConatactDialog/ContactDialogState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Manager.States;
+using Model;
+
+namespace WindowsClient.States.ConatactDialog
+{
+    /// <summary>
+    /// ContactDialogState represents all states of the ConatactDialog
+    /// </summary>
+    class ContactDialogState : IState
+    {
+        public enum ConstactDialofStates
+        {
+            Closed = 0,
+            Canceled = 1,
+            Accepted = 2
+        }
+
+        public int getState()
+        {
+            return 0;
+        }
+    }
+}
