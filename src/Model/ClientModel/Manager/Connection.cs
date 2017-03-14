cceefa74cefdb0a431220e01f3fb100848d64b64
TheMHMoritz3 <mhmoritz3@outlook.com>
2017-03-14 21:32:15 +0100

    Started Work on Connection

---
+++ src/Model/ClientModel/Manager/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Manager;
+using Model.Data;
+
+namespace ClientModel.Manager
+{
+    public class Connection : IConnection
+    {
+
+        Dictionary<LoginData, States.ConnectionState> ConnectionStates;
+    }
+}
---
+++ src/Model/ClientModel/Manager/States/ConnectionState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Manager.States;
+
+namespace ClientModel.Manager.States
+{
+    class ConnectionState : IState
+    {
+        public int getState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
